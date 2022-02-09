e1156c656b065da188b17ac699da88c0e8103974
Your Name <felix.acevedo.tech@gmail.com>
2022-02-08 23:41:37 -0500

    Tony the Robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! Javascript is running!");
+
+var playerName = "Tony the Robot";
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun, " + playerName + "!");
+  }
+  //fight();
