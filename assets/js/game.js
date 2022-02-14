5b2caba2ddaa055e197265d137a5694649d60d48
Your Name <felix.acevedo.tech@gmail.com>
2022-02-13 22:02:23 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,11 +13,11 @@ var playerAttack = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     
@@ -28,20 +28,20 @@ var fight = function() {
       // remove enemy's health by subtracting the amount set in the playerAttack variable
       enemyHealth = enemyHealth - playerAttack;
       console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        playerName + " attacked " + enemyNames+ ". " + enemyNames+ " now has " + enemyHealth + " health remaining."
       );
     
       // check enemy's health
       if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
+        window.alert(enemyNames+ " has died!");
       } else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        window.alert(enemyNames+ " still has " + enemyHealth + " health left.");
       }
     
       // remove player's health by subtracting the amount set in the enemyAttack variable
       playerHealth = playerHealth - enemyAttack;
       console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        enemyNames+ " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
       );
     
       // check player's health
@@ -71,4 +71,6 @@ var fight = function() {
     }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
